5cbcf2c03d712fb8eb4a8d966cb609287f1127d3
Sarbaj <sarbajmalek3456@gmail.com>
2025-06-14 11:57:44 +0530

    updateurl

--- back/index.js
+++ back/index.js
@@ -12,7 +12,7 @@ const app=express()
 app.use(express.json());
 
 app.use(cors( {
-  origin: ['http://localhost:5174','https://sarbazcv.vercel.app'], 
+  origin: ['https://sarbazcv.vercel.app'], 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, 
   optionsSuccessStatus: 204
