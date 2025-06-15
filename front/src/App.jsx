084d19a40de36f042d2aaa7a2928e88df0481a08
Sarbaj <sarbajmalek3456@gmail.com>
2025-06-15 21:54:26 +0530

    scrollji

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Navbar from './PAGE/Navbar'
 import Home from './PAGE/Home'
-import { Routes,Route } from 'react-router-dom';
+import { Routes,Route,useLocation } from 'react-router-dom';
 import Work from './PAGE/Work';
 import About from './PAGE/About';
 import Contact from './PAGE/Contact';
 import Footer from './PAGE/Footer';
 import AddProject from './PAGE/AddProject';
 import Login from './PAGE/Login';
+import { useEffect } from 'react';
 const App = () => {
+    const location = useLocation();
+     useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
   return (
     <>
     <Navbar/>
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
-import ScrollToTop from './PAGE/ScrollToTop.jsx';
+
 
   createRoot(document.getElementById('root')).render(
      <>
-<BrowserRouter>
-<ScrollToTop/>
+    <BrowserRouter>
+     
     <App />
      </BrowserRouter>
  </>
