89b995a6913fb230abb12e19e60ea37c33485456
Sarbaj <sarbajmalek3456@gmail.com>
2025-06-15 15:09:25 +0530

    dbenv

--- back/DB/Db.js
+++ back/DB/Db.js
@@ -1,16 +1,16 @@
 import { connect } from "mongoose";
 
-const DbConnection=async()=>{
+const DbConnection = async () => {
+  try {
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xyevmbo.mongodb.net/${process.env.DB_NAME}`;
+    const Conn = await connect(uri);
 
-    try {
-        const Conn=await connect("mongodb+srv://sarbajmalek3456:ELON_MUSK2499@cluster0.xyevmbo.mongodb.net/cvbackend")
-        if (Conn) {
-            console.log(`✅ Database Connected: ${Conn.connection.host}`);
-        }
-        
-    } catch (error) {   
-        console.error('error on connection');
+    if (Conn) {
+      console.log(`✅ Database Connected: ${Conn.connection.host}`);
     }
+  } catch (error) {
+    console.error('❌ Error on connection:', error.message);
+  }
+};
 
-}
 export default DbConnection;
