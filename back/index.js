f0f1986930f4b8c102eb385f1cf5afdd30aa6a86
sarbazmalek <126250208+Sarbaj@users.noreply.github.com>
2025-07-29 12:40:19 +0530

    Update index.js

--- back/index.js
+++ back/index.js
@@ -13,7 +13,7 @@ const app=express()
 app.use(express.json());
 
 app.use(cors( {
-  origin: ['https://sarbaz.vercel.app'], 
+  origin: ['https://sarbaz.vercel.app','http://localhost:5173'], 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, 
   optionsSuccessStatus: 204
