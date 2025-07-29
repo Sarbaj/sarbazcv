e71380b89cc1907b31e70e97cce69ed8476a6caf
Sarbaj <sarbajmalek3456@gmail.com>
2025-07-29 22:12:29 +0530

    loader

--- front/src/PAGE/Contact.jsx
+++ front/src/PAGE/Contact.jsx
@@ -1,56 +1,89 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 
-import '../STYLE/contact.css';
+import "../STYLE/contact.css";
 
 const Contact = () => {
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  const [message, setMessage] = useState("")
-  const HandleSendmsg=async(e)=>{
-    e.preventDefault()
-    if (!email.includes('@')) {
-      toast.error('Email Is Required')
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const HandleSendmsg = async (e) => {
+    e.preventDefault();
+    if (!email.includes("@")) {
+      toast.error("Email Is Required");
       return;
     }
-       const response = await fetch('https://sarbazcvbackend.vercel.app/bin/contact', {
-               method: 'POST',
-               headers: {
-                   'Content-Type': 'application/json'
-               },
-               body: JSON.stringify({ name:username, email:email,messege:message })
-           });
-           if (response.status==201) {
-             toast.success("Thanks For Connecting.")
-           }
-           if (!response.ok) {
-               const errorData = await response.json();
-               toast.error(errorData.message)
-               
-              }
-    setEmail("")
-    setMessage("")
-    setUsername("")
-  }
+    if (!username || !email || !message) {
+      toast.error("All Fields Are Required");
+      return;
+    }
+    const response = await fetch(
+      "https://sarbazcvbackend.vercel.app/bin/contact",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: username,
+          email: email,
+          messege: message,
+        }),
+      }
+    );
+    if (response.status == 201) {
+      toast.success("Thanks For Connecting.");
+    }
+    if (!response.ok) {
+      const errorData = await response.json();
+      toast.error(errorData.message);
+    }
+    setEmail("");
+    setMessage("");
+    setUsername("");
+  };
   return (
     <>
-    <div className="contact-page">
-      <section className="contact-form-section">
-        <h2>Contact Me</h2>
-        <form className="contact-form">
-          <input type="text" name="name" placeholder="Your Name"  value={username} required onChange={(e)=>setUsername(e.target.value)} />
-          <input type="email" name="email" placeholder="Your Email" value={email} required onChange={(e)=>setEmail(e.target.value)}  />
-          <textarea name="message" rows="5" placeholder="Your Message" value={message} onChange={(e)=>setMessage(e.target.value)} required></textarea>
-          <button  onClick={(e)=>HandleSendmsg(e)}>Send Message</button>
-        </form>
-      </section>
-
-      
-    </div>
-   <ToastContainer style={{zIndex:200}} position="top-right"
-     autoClose={2000} theme="dark" pauseOnHover={false}/>
-     
+      <div className="contact-page">
+        <section className="contact-form-section">
+          <h2>Contact Me</h2>
+          <form className="contact-form">
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              value={username}
+              required
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              value={email}
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <textarea
+              name="message"
+              rows="5"
+              placeholder="Your Message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+            ></textarea>
+            <button onClick={(e) => HandleSendmsg(e)}>Send Message</button>
+          </form>
+        </section>
+      </div>
+      <ToastContainer
+        style={{ zIndex: 200 }}
+        position="top-right"
+        autoClose={2000}
+        theme="dark"
+        pauseOnHover={false}
+      />
     </>
   );
 };
--- front/src/PAGE/Work.jsx
+++ front/src/PAGE/Work.jsx
@@ -1,99 +1,120 @@
-import React, { useState } from 'react'
-import '../STYLE/work.css';
-import { useEffect,useRef  } from 'react';
+import React, { useState } from "react";
+import "../STYLE/work.css";
+import { useEffect, useRef } from "react";
 import { FaLink } from "react-icons/fa";
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-import AI from '../assets/AI.png';
+import gsap from "gsap";
+import { BlinkBlur } from "react-loading-indicators";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import AI from "../assets/AI.png";
 gsap.registerPlugin(ScrollTrigger);
 const Work = () => {
-  const [projectData, setprojectData] = useState([])
-  const FetchProduct=async()=>{
-    try{
-     const response = await fetch('https://sarbazcvbackend.vercel.app/bin/getproject', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        });
-    const data=await response.json()
-    
+  const [projectData, setprojectData] = useState([]);
+  const [load, setload] = useState(true);
+  const FetchProduct = async () => {
+    try {
+      const response = await fetch(
+        "https://sarbazcvbackend.vercel.app/bin/getproject",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
 
-   if (data.messege.length>0) {
-    
-       setprojectData(data.messege)
-   }
-    
+      if (data.messege.length > 0) {
+        setprojectData(data.messege);
+        setload(false);
+      }
+    } catch (err) {
+      setprojectData([]);
     }
-    catch(err){
-      setprojectData([])
-    }
-  }
-useEffect(() => {
-  FetchProduct()
-}, [])
-
-      const imageRef = useRef();
+  };
   useEffect(() => {
-    gsap.utils.toArray('.project-display img').forEach((img) => {
+    FetchProduct();
+  }, []);
+
+  const imageRef = useRef();
+  useEffect(() => {
+    gsap.utils.toArray(".project-display img").forEach((img) => {
       gsap.to(img, {
-        width: '85%',
-        height:'80%',
-        duration:1,
+        width: "85%",
+        height: "80%",
+        duration: 1,
         scrollTrigger: {
           trigger: img,
-          start: 'top 50%',
-          end: 'bottom 20%',
+          start: "top 50%",
+          end: "bottom 20%",
           scrub: true,
         },
       });
     });
   }, [projectData]);
-const Handleproject=(e,i,data)=>{
-  e.preventDefault()
-  if (i==2) {
-    alert("Project Is Under Maintenance. Will Be Live in 2 Days")
-  }
-  else{
-    window.location.href=data.link
-  }
-}
+  const Handleproject = (e, i, data) => {
+    e.preventDefault();
+    if (i == 2) {
+      alert("Project Is Under Maintenance. Will Be Live in 2 Days");
+    } else {
+      window.location.href = data.link;
+    }
+  };
 
   return (
-  <>
-  <div className="work-main">
-    <p>Work</p>
-    <h1>Selected Projects</h1>
-    <div className="project-flex">
+    <>
+      <div className="work-main">
+        <p>Work</p>
+        <h1>Selected Projects</h1>
+        {load && (
+          <BlinkBlur
+            color="#929292"
+            size="large"
+            text="Loading.."
+            textColor=""
+          />
+        )}
 
-    {projectData.length>0 ?(projectData.map((data,i)=>{
-    const stringArray = data.speciality.split(',');
-  const trimmedArray = stringArray.map(item => item.trim());
-  
-  
-    return <div className="project-div" key={i} >
-        <div className="project-display">
-            <img ref={imageRef}  src={data.imageUrl} alt="project img" />
+        <div className="project-flex">
+          {projectData.length > 0 ? (
+            projectData.map((data, i) => {
+              const stringArray = data.speciality.split(",");
+              const trimmedArray = stringArray.map((item) => item.trim());
+
+              return (
+                <div className="project-div" key={i}>
+                  <div className="project-display">
+                    <img ref={imageRef} src={data.imageUrl} alt="project img" />
+                  </div>
+
+                  <h2>{data.name}</h2>
+                  <a
+                    onClick={(e) => Handleproject(e, i, data)}
+                    style={{
+                      textDecoration: "none",
+                      color: "black",
+                      cursor: "pointer",
+                      fontWeight: "500",
+                    }}
+                  >
+                    Live Demo <FaLink />
+                  </a>
+                  <p>{data.description}</p>
+                  <div className="btn-info">
+                    {trimmedArray.length > 0 &&
+                      trimmedArray.map((data, i) => {
+                        return <button key={i}>{data}</button>;
+                      })}
+                  </div>
+                </div>
+              );
+            })
+          ) : (
+            <div></div>
+          )}
         </div>
+      </div>
+    </>
+  );
+};
 
-        <h2>{data.name}</h2>
-        <a onClick={(e)=>Handleproject(e,i,data)} style={{textDecoration:"none",color:"black",cursor:"pointer",fontWeight: "500"}}>Live Demo <FaLink /></a>
-        <p>{data.description}</p>
-        <div className="btn-info">
-          {trimmedArray.length>0 && (trimmedArray.map((data,i)=>{
-            return <button key={i}>{data}</button>
-          }))}
-           
-        </div>
-    </div>
-
-    })):(<div>Loading ....</div>)}
-    
-      
-    </div>
-  </div>
-  </>
-  )
-}
-
-export default Work
+export default Work;
