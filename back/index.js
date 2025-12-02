6929f3ac378e15e09983be9e4b54b1e0002ef71a
Sarbaj <sarbajmalek3456@gmail.com>
2025-12-02 19:03:24 +0530

    fix about

--- back/index.js
+++ back/index.js
@@ -13,7 +13,7 @@ const app=express()
 app.use(express.json());
 
 app.use(cors( {
-  origin: ['https://sarbaz.vercel.app','http://localhost:5173'], 
+  origin: ['https://sarbaz.vercel.app'], 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, 
   optionsSuccessStatus: 204
